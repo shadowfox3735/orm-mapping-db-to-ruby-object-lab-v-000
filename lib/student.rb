6c92fdf8754b559c4c1fbedd6fed9217759d2f4f
shadowfox3735 <normanpan180@gmail.com>
2019-04-08 21:05:08 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -60,6 +60,13 @@ class Student
   end
   
   def self.all_students_in_grade_9
+    sql = <<-SQL
+      SELECT * FROM students
+      WHERE grade == 9
+    SQL
     
+    DB[:conn].execute(sql).each do |row|
+      self.new_from_db(row)
+    end
   end
 end
