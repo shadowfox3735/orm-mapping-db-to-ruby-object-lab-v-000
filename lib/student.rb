cbcc6a3921d2d7fb2ec6480192394bc85118f6b5
shadowfox3735 <normanpan180@gmail.com>
2019-04-08 21:08:59 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -73,7 +73,7 @@ class Student
   def self.students_below_12th_grade
     sql = <<-SQL
       SELECT * FROM students
-      WHERE grade < 12
+      WHERE grade <= 11
     SQL
     
     DB[:conn].execute(sql).each do |row|
