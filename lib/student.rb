e7eac47e61917871016c1ae1eba0809994abb7b3
shadowfox3735 <normanpan180@gmail.com>
2019-04-08 20:27:22 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -16,7 +16,7 @@ class Student
 
   def self.find_by_name(name)
     sql = <<-SQL
-      SELECT * FROM students WHERE
+      SELECT * FROM students WHERE name = ?
     SQL
 
     DB[:conn].execute(sql, self.name, self.grade)
