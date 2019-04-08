9dba0d92d510815d9e80815a73fbb2136b255a50
shadowfox3735 <normanpan180@gmail.com>
2019-04-08 20:33:57 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -20,7 +20,9 @@ class Student
       LIMIT 1
     SQL
 
-    DB[:conn].execute(sql, name)
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
   end
   
   def save
